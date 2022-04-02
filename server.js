900d3177dc8fd68ff895e77eafb1b1e93ec19802
piyush1795 <chandra.piyush17@gmail.com>
2022-04-02 16:42:45 +0530

    make it better

--- server.js
+++ server.js
@@ -15,12 +15,10 @@ app.use("/", contactRoute)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
-    app.length("*", (req,res) => 
-        res.sendFile(path.res)
+    app.get("*", (req,res) => 
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     )
 }
-
-
 
 const port = process.env.PORT || 5000
 app.listen(port,console.log(`server listening to port 5000`))
